71b0d7c82a438ff9079aa6355a8dcc357e48d363
Da-ani <daniellesarer93@hotmail.com>
2019-01-17 13:20:30 -0600

    Ya pasó los test

--- src/cipher.js
+++ src/cipher.js
@@ -3,31 +3,57 @@ window.cipher = {
 // esta es una funcion y se le van a pasar los parametros, dentro de esta función
 //le daremos las instrucciones a encode
 encode: (offsetIngresado, textoIngresado) => {
-let recibir = document.getElementById('receptordemensaje')
-let nuevaPalabra= "";
-let i = 0;
-textoIngresado= textoIngresado.toUpperCase();
+  let nuevaPalabra= "";
+  let i = 0;
+  textoIngresado= textoIngresado.toUpperCase();
 
-//Hacer un for para que vaya contando las letras que ingrese el usuario de una en una
-for(i=0; i<textoIngresado.length; i++){
-  //usamos charcodeat para que me convierta las letras ingresadas a ascii
-let textoYaCifrado = (textoIngresado.charCodeAt(i) - 65 + parseInt(offsetIngresado)) %26 + 65;
-//como en elresultado de la formula solo me dará en que posición estará
-// la letra del desplazamiento que dimos usamos String.fromCharCodeF
-//para que esa posición del desplazamiento que pedimos me lo convierta a la letra que le corresponde
-let resultado = String.fromCharCode(textoYaCifrado);
+  //Hacer un for para que vaya contando las letras que ingrese el usuario de una en una
+  for(i=0; i<textoIngresado.length; i++){
+      //usamos charcodeat para que me convierta las letras ingresadas a ascii
+    let textoYaCifrado = (textoIngresado.charCodeAt(i) - 65 + parseInt(offsetIngresado)) %26 + 65;
+    //como en elresultado de la formula solo me dará en que posición estará
+    // la letra del desplazamiento que dimos usamos String.fromCharCodeF
+    //para que esa posición del desplazamiento que pedimos me lo convierta a la letra que le corresponde
+    let resultado = String.fromCharCode(textoYaCifrado);
 
-nuevaPalabra+=resultado;
+    nuevaPalabra+=resultado; //Esto significa que como el for va dando vueltas por cada letra ingresada por el usuario,
+    // en let resultado va mandando
+    //las letras que va pasando, por ejemplo si el ususario pone ABC, lo que hace for es primero ir con la  A
+    //le aplica toda la función y la manda a let resultado despues se sigue con la B, le aplica la funcion y al momento de mandarla
+    //a let resultado esa B va a sustituir a la A y en realidad no queremos eso lo que queremos es que asi como vaya
+    //pasando las letras las vaya guardando y es por ello que creamos una variable llamada let nuevaPalabra
+    //dandole un valor ("") vacío con comillas de manera que la computadora sepa con esas comillas que ahí se va a guardar
+    //un string
+    //En donde dice (nuevaPalabra+=resultado) lo que estamos haciendo es: que en la variable nuevaPalabra que ya está
+    //declarada en la parte de arriba con el + le estamos sumando cada letra que va contando el for y la va mandando
+    // a la variable declarada arriba como let nuevaPalabra
 
-}
-recibir.value= nuevaPalabra
-return nuevaPalabra
+    }
+  return nuevaPalabra
+
+
 },
 
 
 
-decode: () => {
+decode: (offsetIngresado, textoIngresado) => {
 
+
+  let nuevaPalabra= "";
+  let i = 0;
+  textoIngresado= textoIngresado.toUpperCase();
+
+  for(i=0; i<textoIngresado.length; i++){
+
+    let textoYaCifrado = (textoIngresado.charCodeAt(i) + 65 - parseInt(offsetIngresado)) %26 + 65;
+
+    let resultado = String.fromCharCode(textoYaCifrado);
+
+    nuevaPalabra+=resultado;
+
+  }
+
+  return nuevaPalabra
 
 }
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
+// let recibir= document.getElementById('receptordemensaje')
+//let recibir= (window.cipher.encode(offsetIngresado, textoIngresado)
+
+let recibir1= document.getElementById('receptordemensaje')
+
 document.getElementById('flechacifrar').addEventListener('click',() => {
 let textoIngresado = document.getElementById('mensaje').value;
 
 let offsetIngresado = document.getElementById('offset').value;
 
-window.cipher.encode(offsetIngresado, textoIngresado);
+//(window.cipher.encode(offsetIngresado, textoIngresado);
+//let recibir= document.getElementById('receptordemensaje').(window.cipher.encode(offsetIngresado, textoIngresado);
+let funcion1= window.cipher.encode(offsetIngresado, textoIngresado);
+recibir1.value= funcion1;
 
 });
 
+let recibir2= document.getElementById('receptordemensaje')
 
-// document.getElementById('flechadescifrar').addEventListener('click',() => {
-//
-// let textoIngresado = document.getElementById('mensaje').value;
-//
-// let offsetIngresado = document.getElementById('offset').value;
-//
-// window.cipher.decode(offsetIngresado, textoIngresado);
-//
-// });
+document.getElementById('flechadescifrar').addEventListener('click',() => {
+
+let textoIngresado = document.getElementById('mensaje').value;
+
+let offsetIngresado = document.getElementById('offset').value;
+
+let funcion2= window.cipher.decode(offsetIngresado, textoIngresado);
+recibir2.value= funcion2;
+ });
